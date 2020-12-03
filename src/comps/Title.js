1a226429969ccc348da08b12bcd8975bb176a712
alan Dittler <dittler.a@gmail.com>
2020-12-03 17:10:06 -0300

    logo

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1 className="firegram">FireGram</h1>
+      <div className="user-info-container">
+        <img
+          className="profile-picture-user"
+          src="https://pbs.twimg.com/profile_images/1331420928094445571/mfIKS5EH_400x400.jpg"
+          alt="user"
+        ></img>
+        <div className="user-info">
+          <h1>Alan Miranda</h1>
+          <div className="account-stats">
+            <div className="states">
+              <span>9</span>
+              <br></br>
+              <span>Publications</span>
+            </div>
+            <div className="states">
+              <span>155</span>
+              <br></br>
+              <span>Followers</span>
+            </div>
+            <div className="states">
+              <span>200</span>
+              <br></br>
+              <span>Following</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
